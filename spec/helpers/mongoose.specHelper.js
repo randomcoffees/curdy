630453897e20e06ba3c1ab1c923e709179100f94
Edward Andrew Robinson <earobinson@gmail.com>
2017-11-13 13:05:36 -0500

    console.log debugging

--- spec/helpers/mongoose.specHelper.js
+++ spec/helpers/mongoose.specHelper.js
@@ -2,19 +2,25 @@ const Q = require('q');
 const mongoose = require('mongoose');
 const Mockgoose = require('mockgoose').Mockgoose;
 const mockgoose = new Mockgoose(mongoose);
+console.log(-1);
 
 before(() => {
+  console.log(0);
   return mockgoose.prepareStorage()
   .then(() => {
+    console.log(1);
     return mongoose.connection.close();
   })
   .then(() => {
+    console.log(2);
     return mongoose.connect('mongodb://example.com/TestingDB');
   });
 });
 
 beforeEach(() => {
+  console.log(3);
   return Q.when(Object.keys(mongoose.connection.models).forEach(modelKey => {
+    console.log(4);
     delete mongoose.connection.models[modelKey];
   }));
 });
