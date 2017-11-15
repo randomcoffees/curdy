0175f841e21c7095874ceb21064881dbdb06ebad
Edward Andrew Robinson <earobinson@gmail.com>
2017-11-15 11:21:26 -0500

    installed mongodb-download@2.2.3 to resolve timeout issue
    - per https://github.com/Mockgoose/Mockgoose/issues/64
    - disabled emails
    - added npm caching dir
    - cleaned up linting

--- spec/helpers/mongoose.specHelper.js
+++ spec/helpers/mongoose.specHelper.js
@@ -1,35 +1,19 @@
-const Q = require('q');
 const mongoose = require('mongoose');
 const Mockgoose = require('mockgoose').Mockgoose;
 const mockgoose = new Mockgoose(mongoose);
-console.log(-1);
 
 before(() => {
-  console.log(0);
-  try {
-    return Q.when(mockgoose.prepareStorage())
-    .then(() => {
-      console.log(1);
-      return Q.when(mongoose.connection.close());
-    })
-    .then(() => {
-      console.log(2);
-      return Q.when(mongoose.connect('mongodb://example.com/TestingDB'));
-    })
-    .catch(err => {
-      console.log(err);
-      throw err;
-    });
-  } catch(err) {
-    console.log(err);
-    throw err;
-  }
+  return mockgoose.prepareStorage()
+  .then(() => {
+    return mongoose.connection.close();
+  })
+  .then(() => {
+    return mongoose.connect('mongodb://example.com/TestingDB');
+  });
 });
 
 beforeEach(() => {
-  console.log(3);
-  return Q.when(Object.keys(mongoose.connection.models).forEach(modelKey => {
-    console.log(4);
+  Object.keys(mongoose.connection.models).forEach(modelKey => {
     delete mongoose.connection.models[modelKey];
-  }));
+  });
 });
