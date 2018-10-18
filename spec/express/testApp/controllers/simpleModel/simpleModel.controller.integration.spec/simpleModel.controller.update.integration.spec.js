8e572a4cc639a9e0b2a59eecf2bfb684b634809a
Edward Andrew Robinson <earobinson@gmail.com>
2018-10-18 14:16:59 -0400

    wip

--- spec/express/testApp/controllers/simpleModel/simpleModel.controller.integration.spec/simpleModel.controller.update.integration.spec.js
+++ spec/express/testApp/controllers/simpleModel/simpleModel.controller.integration.spec/simpleModel.controller.update.integration.spec.js
@@ -4,7 +4,7 @@ const request = require('supertest');
 const expect = chai.expect;
 
 const SimpleModel = require('../../../../../models/simpleModel.model');
-const expressIntegrationHelper = require('./../../../express.integrationHelper');
+const expressIntegrationHelper = require('../../../express.integrationHelper');
 
 describe('simpleModel.controller.update.integration.spec', () => {
   beforeEach(async () => {
@@ -19,7 +19,7 @@ describe('simpleModel.controller.update.integration.spec', () => {
     });
   });
 
-  it('must patch a SimpleModel', async () => {
+  it('must update a SimpleModel', async () => {
     const response = await request(this.app)
     .put(`/simpleModel/${this.simpleModel._id}`)
     .send({
@@ -27,17 +27,18 @@ describe('simpleModel.controller.update.integration.spec', () => {
       boolean: false
     });
 
+    // console.log({ response });
     expect(response.statusCode).to.equal(200);
 
     expect(response.body._id).to.equal(this.simpleModel._id.toString());
     expect(response.body.string).to.equal('not string');
-    expect(response.body.number).to.equal(null);
+    expect(response.body.number).to.equal(42);
     expect(response.body.boolean).to.equal(false);
 
-    const simpleModel = await SimpleModel.findById(response.body._id);
+    const simpleModel = await SimpleModel.findById(this.simpleModel._id);
 
     expect(simpleModel.string).to.equal('not string');
-    expect(simpleModel.number).to.equal(null);
+    expect(simpleModel.number).to.equal(42);
     expect(simpleModel.boolean).to.equal(false);
   });
 });
